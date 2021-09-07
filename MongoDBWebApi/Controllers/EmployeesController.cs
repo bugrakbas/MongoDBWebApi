7c65eb15e24d3c3e0c50264384b9e8c91301144e
ŞAMİL BUĞRAHAN AKBAŞ <2018123025@cumhuriyet.edu.tr>
2021-09-07 10:25:48 +0300

    Add project files.

---
+++ MongoDBWebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDBWebApi.Models;
+using MongoDBWebApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDBWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+        private readonly EmployeeService _employeeService;
+        public EmployeesController(EmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+        [HttpGet]
+        public ActionResult<List<Employee>> Get() => _employeeService.Get();
+        [HttpGet("{id:length(24)}",Name ="GetEmployee")]
+        public ActionResult<Employee> Get(string id)
+        {
+            var emp = _employeeService.Get(id);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            return emp;
+                
+        }
+
+
+    }
+}
---
+++ MongoDBWebApi/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDBWebApi.Models
+{
+    public class Employee
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
